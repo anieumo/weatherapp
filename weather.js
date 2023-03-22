1cae8ece7d13fed6116597b7358bc5536edc526c
Aniedi Umoren <102189494+anieumo@users.noreply.github.com>
2023-03-22 09:20:59 -0400

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,21 +1,38 @@
 function returnCity(){
-   city = document.getElementById("userInput").value;
-   alert("userInput");
+    if (document.getElementById("userInput").value.length === 0) {
+        alert("userInput")
+    } else {
+        city = document.getElementById("userInput").value;
+    }
+    // city = document.getElementById("userInput").value;
+    // alert("userInput");
 }
  
 function getweather(city) { 
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city "&appid=482d2f31dc8b7beafbff108ae04a732");
-        .then(response) => response.json(); /* package response as json*/
-        .then(data) => console.log(data); /* rest API*/
-    }
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=482d2f31dc8b7beafbff108ae04a732")
+        .then(response) => {
+            if (!response.ok) {
+                alert("No weather found")
+                throw new Error("no weather found")
+            }
+            response.json()
+        }
+        .then(data) => weatherdisplay(); /* rest API*/
 }
 
 /* take a location and return the weather data for that location*/
 /*set up a form that lets users input their location and fetches above info*/
-function weatherdisplay(data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    alert(name, icon, description, temp, humidity, speed);
-}
+// function weatherdisplay(data) {
+//     const { name } = data;
+//     const { icon, description } = data.weather[0];
+//     const { temp, humidity } = data.main;
+//     const { speed } = data.wind;
+//     console.log(name, icon, description, temp, humidity, speed);
+
+//     document.getElementById("city").innerText = "Weather in " + name;
+//     document.getElementById("temp").innerText = temp + "°C";
+//     document.getElementById("description").innerText = description;
+//     document.getElementById("humidity").innerText = humidity;
+//     document.getElementById("wind").innerText = speed;
+
+// }
